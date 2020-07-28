d16007a1a094a92944bb5ed69e06f62f00d9ea72
Cesar Quintana <cesar92id@gmail.com>
2020-07-27 21:09:48 -0400

    Updates

---
+++ precache-manifest.f967e4bdc22444ef8238d05dfef5c3d5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be5f2c1d4e5facd6a79ca5331fbe3255",
+    "url": "/twittor-cliente/index.html"
+  },
+  {
+    "revision": "d1c9b8fb28fad1bb6db7",
+    "url": "/twittor-cliente/static/css/2.ff572f47.chunk.css"
+  },
+  {
+    "revision": "04710fec9afee33cdc8e",
+    "url": "/twittor-cliente/static/css/main.cf00e601.chunk.css"
+  },
+  {
+    "revision": "d1c9b8fb28fad1bb6db7",
+    "url": "/twittor-cliente/static/js/2.a87209c9.chunk.js"
+  },
+  {
+    "revision": "8bb92c23487eb5c952e51992947cb95e",
+    "url": "/twittor-cliente/static/js/2.a87209c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04710fec9afee33cdc8e",
+    "url": "/twittor-cliente/static/js/main.43b5d3aa.chunk.js"
+  },
+  {
+    "revision": "8fd1180534acf1a27931",
+    "url": "/twittor-cliente/static/js/runtime-main.527d6e71.js"
+  },
+  {
+    "revision": "16e35662baa4ca9e4f4344fd51fb7ec6",
+    "url": "/twittor-cliente/static/media/camara.16e35662.svg"
+  },
+  {
+    "revision": "cad7008135a58fda8ec20be42808a6b2",
+    "url": "/twittor-cliente/static/media/close.cad70081.svg"
+  },
+  {
+    "revision": "9b45fe492d3b935b1a283ece2e1b5ac8",
+    "url": "/twittor-cliente/static/media/date-birth.9b45fe49.svg"
+  },
+  {
+    "revision": "d3c57e3b34eea06328b31dbb81bcabc4",
+    "url": "/twittor-cliente/static/media/error-404.d3c57e3b.png"
+  },
+  {
+    "revision": "a108520eea84cd603eeaf8eeda7676b1",
+    "url": "/twittor-cliente/static/media/link.a108520e.svg"
+  },
+  {
+    "revision": "02f2070b4161235fe7d8909276457b53",
+    "url": "/twittor-cliente/static/media/location.02f2070b.svg"
+  },
+  {
+    "revision": "41f65f75cb0b41db6fe44ab4b074bacc",
+    "url": "/twittor-cliente/static/media/logo-white.41f65f75.png"
+  },
+  {
+    "revision": "1769587d7e1330e9d4241c30f672c7ff",
+    "url": "/twittor-cliente/static/media/logo.1769587d.png"
+  }
+]);
